a81a10b445d92dbd8bae8a7a47aa4c992226c223
Gooey Nyan <Sanjwinds@gmail.com>
2017-12-14 01:35:45 +0800

    update index.ts, currify the logic

--- build/index.js
+++ build/index.js
@@ -4,31 +4,34 @@ const EventHub_1 = require("./EventHub");
 const inputEl = document.getElementById("input");
 const buttonA = document.getElementById("buttonA");
 const buttonB = document.getElementById("buttonB");
-const getCoffee = () => {
+const buttonC = document.getElementById("buttonC");
+const promiseSomething = (somthing, time) => {
     return new Promise(resolve => {
-        setTimeout(() => resolve("☕️"), 3000);
+        setTimeout(() => resolve(somthing), time);
     });
 };
-const getHamburger = () => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve("🍔"), 1000);
-    });
+const getCoffee = () => promiseSomething("☕️", 1500);
+const getHamburger = () => promiseSomething("🍔", 500);
+const getHeart = () => promiseSomething("❤️", 1000);
+const handleWrapper = (f) => async (...args) => {
+    console.log(`${f.name} start.`);
+    inputEl.value = await f();
+    console.log(`${f.name} end.`);
 };
-const handleClickOnButtonA = async (...arg) => {
-    inputEl.value = await getCoffee();
-    console.log(arg);
-};
-const handleClickOnButtonB = async (...arg) => {
-    inputEl.value = await getHamburger();
-    console.log(arg);
-};
+const handleClickOnButtonA = handleWrapper(getCoffee);
+const handleClickOnButtonB = handleWrapper(getHamburger);
+const handleClickOnButtonC = handleWrapper(getHeart);
 const eventHub = new EventHub_1.default();
 eventHub.on("clickOnA", handleClickOnButtonA);
 eventHub.on("clickOnB", handleClickOnButtonB);
-buttonA.addEventListener("click", () => {
-    eventHub.emit("clickOnA", `dasd`, `sadf`);
-});
-buttonB.addEventListener("click", () => {
-    eventHub.emit("clickOnB", 123, 312);
-});
+eventHub.on("clickOnC", handleClickOnButtonC);
+const bindClickEvent = (el, event, ...args) => {
+    el.addEventListener("click", () => {
+        eventHub.emit(event, ...args);
+    });
+};
+bindClickEvent(buttonA, "clickOnA", `A`, `A`);
+bindClickEvent(buttonA, "clickOnA", 1, 1);
+bindClickEvent(buttonB, "clickOnB", `B`, 2);
+bindClickEvent(buttonC, "clickOnC", `C`, 3, 2, 1);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,42 @@ import EventHub from "./EventHub";
 const inputEl: HTMLElement = <HTMLElement>document.getElementById("input");
 const buttonA: HTMLElement = <HTMLElement>document.getElementById("buttonA");
 const buttonB: HTMLElement = <HTMLElement>document.getElementById("buttonB");
+const buttonC: HTMLElement = <HTMLElement>document.getElementById("buttonC");
 
-const getCoffee = () => {
+const promiseSomething = (somthing: any, time: number) => {
   return new Promise(resolve => {
-    setTimeout(() => resolve("☕️"), 3000);
+    setTimeout(() => resolve(somthing), time);
   });
 };
 
-const getHamburger = () => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve("🍔"), 1000);
-  });
+const getCoffee = () => promiseSomething("☕️", 1500);
+
+const getHamburger = () => promiseSomething("🍔", 500);
+
+const getHeart = () => promiseSomething("❤️", 1000);
+
+const handleWrapper = (f: Function) => async (...args) => {
+  console.log(`${f.name} start.`);
+  inputEl.value = await f();
+  console.log(`${f.name} end.`);
 };
 
-const handleClickOnButtonA = async (...arg) => {
-  inputEl.value = await getCoffee();
-  console.log(arg);
-};
-
-const handleClickOnButtonB = async (...arg) => {
-  inputEl.value = await getHamburger();
-  console.log(arg);
-};
+const handleClickOnButtonA = handleWrapper(getCoffee);
+const handleClickOnButtonB = handleWrapper(getHamburger);
+const handleClickOnButtonC = handleWrapper(getHeart);
 
 const eventHub = new EventHub();
 eventHub.on("clickOnA", handleClickOnButtonA);
 eventHub.on("clickOnB", handleClickOnButtonB);
+eventHub.on("clickOnC", handleClickOnButtonC);
 
-buttonA.addEventListener("click", () => {
-  eventHub.emit("clickOnA", `dasd`, `sadf`);
-});
-buttonB.addEventListener("click", () => {
-  eventHub.emit("clickOnB", 123, 312);
-});
+const bindClickEvent = (el: HTMLElement, event: string, ...args: any[]) => {
+  el.addEventListener("click", () => {
+    eventHub.emit(event, ...args);
+  });
+};
+
+bindClickEvent(buttonA, "clickOnA", `A`, `A`);
+bindClickEvent(buttonA, "clickOnA", 1, 1);
+bindClickEvent(buttonB, "clickOnB", `B`, 2);
+bindClickEvent(buttonC, "clickOnC", `C`, 3, 2, 1);
